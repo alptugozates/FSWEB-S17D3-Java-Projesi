4b1f6f6d0f18c8013a4f8be443e234203c29fea1
alptugozates <omeralptugozates@gmail.com>
2024-02-08 17:31:40 +0300

    Proje tamamlandı

--- Rest-API/src/main/java/com/workintech/zoo/exceptions/KangarooNotFoundException.java
+++ Rest-API/src/main/java/com/workintech/zoo/exceptions/KangarooNotFoundException.java
@@ -1,5 +1,8 @@
 package com.workintech.zoo.exceptions;
 
+import lombok.Data;
+
+@Data
 public class KangarooNotFoundException extends RuntimeException{
     public KangarooNotFoundException(String message) {
         super(message);
--- Rest-API/src/main/java/com/workintech/zoo/exceptions/KoalaNotFoundException.java
+++ Rest-API/src/main/java/com/workintech/zoo/exceptions/KoalaNotFoundException.java
@@ -1,5 +1,8 @@
 package com.workintech.zoo.exceptions;
 
+import lombok.Data;
+
+@Data
 public class KoalaNotFoundException extends RuntimeException{
     public KoalaNotFoundException(String message) {
         super(message);
